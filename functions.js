ef970735c400671eaecf7add1cc9cc2e6029c8d7
Stetson Done <scdone@gmail.com>
2022-01-10 12:51:12 -0700

    steps 1-4 and challenge completed

--- functions.js
+++ functions.js
@@ -22,11 +22,27 @@ function add(num1, num2){
     return num1 + num2
 }
 
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function divide(num1, num2){
+    return num1/num2
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+
 
 
 
 module.exports = {
     returnTwo,
     greeting,
-    add
+    add,
+    multiply,
+    divide,
+    subtract
 }
--- functions.test.js
+++ functions.test.js
@@ -2,7 +2,10 @@
 const {
     returnTwo,
     greeting,
-    add
+    add,
+    multiply,
+    divide,
+    subtract
 } = require('./functions')
 
 //example =     test('first name should be Patten', () => {
@@ -16,17 +19,40 @@ const {
 // })
 
 
-test('this test should return 2', () => {
-    let result = returnTwo()
-    expect(result).toEqual(2)
-})
+// test('this test should return 2', () => {
+//     let result = returnTwo()
+//     expect(result).toEqual(2)
+// })
 
-test('this test should return proper greeting', () => {
-    let testGreeting = greeting('Shelby')
-    expect(testGreeting).toEqual('Hello, Shelby')
-})
+// test('this test should return proper greeting', () => {
+//     let testGreeting = greeting('Shelby')
+//     expect(testGreeting).toEqual('Hello, Shelby')
+// })
 
-test('this test should add two numbers', () => {
-    let testNums = add(2,2)
-    expect(testNums).toEqual(4)
+// test('this test should add two numbers', () => {
+//     let testNums = add(2,2)
+//     expect(testNums).toEqual(4)
+// })
+
+describe (`Math functions tests`, () => {
+
+    test('This test should add two numbers', () => {
+        let testAdd = add(2,2)
+        expect(testAdd).toEqual(4)
+    }),
+
+    test(`This test should multiply two numbers`, () => {
+        let testMultiply = multiply(2,3)
+        expect(testMultiply).toEqual(6)
+    }),
+
+    test(`This test should divide two numbers`, () => {
+        let testDivide = divide(10,5)
+        expect(testDivide).toEqual(2)
+    }),
+
+    test('This test should subtract two numbers', () => {
+        let testSubtract = subtract(100,50)
+        expect(testSubtract).toEqual(50)
+    })
 })
